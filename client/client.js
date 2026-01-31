c580aa047c781f35b15930fd2696ffad6a9aab7d
Joseph Hall <joseph.hall@bristol.ac.uk>
2026-01-31 11:34:00 +0000

    client connects to the server

--- client/client.js
+++ client/client.js
@@ -1,14 +1,19 @@
 // Client-side code using websockets. no idea what i'm doing
 
 window.onload = () => {
-    const wsUri = "ws://127.0.0.1/";
+    const wsUri = "ws://127.0.0.1:8000/";
     const websocket = new WebSocket(wsUri);
 
     websocket.addEventListener("open", () => {
-        log("CONNECTED");
-        pingInterval = setInterval(() => {
-            log(`SENT: ping: ${counter}`);
+        console.log("CONNECTED");
+        var counter = 0
+        const pingInterval = setInterval(() => {
+            console.log(`SENT: ping: ${counter}`);
+            counter += 1
             websocket.send("ping");
         }, 1000)
     })
 }
+
+// when player moves, send updated position to server. send to everyone (and receive all of theirs) 
+// and draw all characters
