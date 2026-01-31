db808d8f9ea36b343305120f19439affbee0374d
Joseph Hall <joseph.hall@bristol.ac.uk>
2026-01-31 12:28:04 +0000

    created Connection class, has method to send the player position to the server

--- client/client.js
+++ client/client.js
@@ -1,19 +1,54 @@
 // Client-side code using websockets. no idea what i'm doing
 
-window.onload = () => {
-    const wsUri = "ws://127.0.0.1:8000/";
-    const websocket = new WebSocket(wsUri);
+class Connection {
+    constructor(uri) {
+        this.wsUri = uri;
+        this.websocket = new WebSocket(this.wsUri);
+        
+        // Listen fo errors and log (for now)
+        this.websocket.addEventListener("error", (e) => {
+            console.log(`ERROR: ${e}`)
+        })
+        
+        websocket.addEventListener("message", (e) => {
+            console.log(`RECEIVED: ${e.data}: ${counter}`);
+            counter++;
+        });
 
-    websocket.addEventListener("open", () => {
-        console.log("CONNECTED");
-        var counter = 0
-        const pingInterval = setInterval(() => {
-            console.log(`SENT: ping: ${counter}`);
-            counter += 1
-            websocket.send("ping");
-        }, 1000)
-    })
+        // TODO: Get the connection ID from the server (somehow)
+        this.connection_id = 0
+
+    }
+
+    // Keep pinging the server
+    ping() {
+        this.websocket.addEventListener("open", () => {
+            console.log("CONNECTED");
+            var counter = 0
+            const pingInterval = setInterval(() => {
+                console.log(`SENT: ping: ${counter}`);
+                counter += 1
+                this.websocket.send("ping");
+            }, 1000)
+        })
+    }
+
+    // Send the player's position to the server
+    send(player) {
+        const update = {
+            player_id: this.connection_id,
+            content: {
+                x_pos: player.x,
+                y_pos: player.y,
+                orientation: player.orientation,
+                moving: player.drawstate,
+                mask: player.mask
+            }
+        }   
+        this.websocket.send(JSON.stringify(update))
+    }
 }
 
+export { Connection }
 // when player moves, send updated position to server. send to everyone (and receive all of theirs) 
 // and draw all characters
