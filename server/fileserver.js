736bfb2e96949579a38e8891256871653966648e
fjebaker <fergus@cosroe.com>
2026-01-31 00:38:57 +0000

    feat(server): websockets working

--- server/fileserver.js
+++ server/fileserver.js
@@ -1,20 +1,20 @@
 const fs = require("fs");
 const http = require("http");
 const path = require("path");
-const WebSocketServer = require("ws");
+const ws = require("ws");
 
 const PORT = 8000;
 
 const MIME_TYPES = {
-  default: "application/octet-stream",
-  html: "text/html; charset=UTF-8",
-  js: "text/javascript",
-  css: "text/css",
-  png: "image/png",
-  jpg: "image/jpeg",
-  gif: "image/gif",
-  ico: "image/x-icon",
-  svg: "image/svg+xml",
+    default: "application/octet-stream",
+    html: "text/html; charset=UTF-8",
+    js: "text/javascript",
+    css: "text/css",
+    png: "image/png",
+    jpg: "image/jpeg",
+    gif: "image/gif",
+    ico: "image/x-icon",
+    svg: "image/svg+xml",
 };
 
 const STATIC_PATH = path.join(process.cwd(), "client");
@@ -23,51 +23,44 @@ const toBool = [() => true, () => false];
 
 // Translates a url path to a file name.
 function lookupPath(url_path) {
-  if (url_path == "/") return "index.html";
-  return url_path;
+    if (url_path == "/") return "index.html";
+    return url_path;
 }
 
 async function requestHandler(req, res) {
-  const file_path = path.join(STATIC_PATH, lookupPath(req.url));
-  console.log(file_path);
-  const file_exists = await fs.promises.access(file_path).then(...toBool);
+    if (req.url == "/ws") return; // it's already been upgraded
 
-  var status_code = 200;
-  if (file_exists) {
-    const ext = path.extname(file_path).substring(1).toLowerCase();
-    const stream = fs.createReadStream(file_path);
-    const mimeType = MIME_TYPES[ext] || MIME_TYPES.default;
-    res.writeHead(status_code, { "Content-Type": mimeType });
-    stream.pipe(res);
-  } else {
-    status_code = 404;
-    res.writeHead(status_code);
-    res.end("Not found\n");
-  }
+    const file_path = path.join(STATIC_PATH, lookupPath(req.url));
+    console.log(file_path);
+    const file_exists = await fs.promises.access(file_path).then(...toBool);
 
-  console.log(`${req.method} ${req.url} ${status_code}`);
+    var status_code = 200;
+    if (file_exists) {
+        const ext = path.extname(file_path).substring(1).toLowerCase();
+        const stream = fs.createReadStream(file_path);
+        const mimeType = MIME_TYPES[ext] || MIME_TYPES.default;
+        res.writeHead(status_code, { "Content-Type": mimeType });
+        stream.pipe(res);
+    } else {
+        status_code = 404;
+        res.writeHead(status_code);
+        res.end("Not found\n");
+    }
+
+    console.log(`${req.method} ${req.url} ${status_code}`);
 }
 
 const server = http.createServer(requestHandler);
 
-const wss = new WebSocketServer({server});
+const wss = new ws.WebSocketServer({ server });
 
-wss.on('connection', function connection(ws) {
-    ws.on('error', console.error)
-})
+wss.on("connection", function connection(ws) {
+    console.log("Websocket connected");
+    ws.on("error", console.error);
 
-server.on('upgrade', function upgrade(request, socket, head){
-    const {pathname} = new URL(request.url, `ws://127.0.0.1:${PORT}/`);
-    if (pathname === 'foo') {
-        wss.handleUpgrade(request, socket, head, function done(ws) {
-            wss.emit('connection', ws, request);
-        })
-    } else {
-        socket.destroy();
-    }
-})
+    ws.send("Welcome, traveler.");
+});
 
 server.listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}/`);
-
