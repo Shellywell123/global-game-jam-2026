732b660036234c22f97565b12a58c0008c6cca4d
JR-Mitchell <8377714+JR-Mitchell@users.noreply.github.com>
2026-02-01 11:05:07 +0000

    feat: updates so that enemies are the same colour as their masks

--- client/state.js
+++ client/state.js
@@ -57,21 +57,25 @@ class State {
 
     // Entry point to start the game
     async start() {
-        const all_assets = await Promise.all([
+	const asset_promises = [
             loadPlayerSprites(this.assets),
-            loadPlayerSprites(this.assets, {
-                character: "enemy",
-                tint_key: "arlecchino",
-            }),
             loadAllMaskSprites(this.assets),
             loadAllMaskSprites(this.assets, {
                 character: "enemy",
-            }),
-        ]);
+            })
+        ];
+        config.MASK_CONFIG.forEach((conf) => {
+            asset_promises.push(
+                loadPlayerSprites(this.assets, {
+                    character: "enemy",
+                    tint_key: conf[0],
+                })
+            );
+	});
+        const all_assets = await Promise.all(asset_promises);
         const character_sprites = all_assets[0];
-        const enemy_sprites = all_assets[1];
-        const character_masks = all_assets[2];
-        const enemy_masks = all_assets[3];
+        const character_masks = all_assets[1];
+        const enemy_masks = all_assets[2];
 
         const map_index = await this.assets.fetchFile(
             "/assets/maps/asscii-map1.txt",
@@ -79,8 +83,8 @@ class State {
         // set and load the game map
         this.game_map.setMap(this.assets.file_buffer[map_index]);
 
-        this.addCharacter = () => {
-            this.characters.push(new Character(enemy_sprites, enemy_masks));
+        this.addCharacter = (character) => {
+            this.characters.push(new Character(all_assets[3 + character.mask], enemy_masks));
         };
 
         this.addPlayer = () => {
@@ -115,14 +119,15 @@ class State {
         // Update the characters (enemies)
         if (message.characters !== undefined) {
             var n_char = this.characters.length;
+	    let curr_characters_length = this.characters.length;
             let new_characters =
-                message.characters.length - this.characters.length;
+                message.characters.length - curr_characters_length;
 
             if (new_characters < 0) {
                 console.error("Missing characters");
             } else if (new_characters > 0) {
                 for (let i = 0; i < new_characters; i++) {
-                    this.addCharacter();
+                    this.addCharacter(message.characters[curr_characters_length + i]);
                 }
             }
 
